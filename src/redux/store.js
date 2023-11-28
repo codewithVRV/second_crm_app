9370fa15a96016edfeaa9510db5c0da99a604292
Vishnu Kumar <vermaji1891@gmail.com>
2023-11-28 14:37:43 +0530

    added our first asynthunk for login

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,7 @@ const store = configureStore({
     reducer:{
         auth: authSliceReducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}),
     devTools: true,
 })
 
