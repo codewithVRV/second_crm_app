81a77d30709972a68b5b35fa9b3c6c83a59985e8
Vishnu Kumar <vermaji1891@gmail.com>
2023-11-28 11:34:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+// import Login from './pages/auth/Login'
+import Singup from './pages/auth/Signup'
+
+function App() {
+
+  return (
+    <>
+      <Singup />
+      {/* <Login /> */}
+    </>
+  )
+}
+
+export default App
