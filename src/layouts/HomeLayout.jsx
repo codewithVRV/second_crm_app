48b7db88bce06051e616ac9d030c923cd7ee5eba
Vishnu Kumar <vermaji1891@gmail.com>
2023-11-30 18:43:41 +0530

    added tickets dashboard

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -33,7 +33,7 @@ function HomeLayout ({children}) {
                 <div className="drawer-side">
                     <label htmlFor="my-drawer" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-                        <li><a>View All Tickets</a></li>
+                        <li><Link to={"/"}>Home</Link></li>
                         <li><Link to={"/dashboard"}>Dashboard</Link></li>
 
                         <li className='absolute bottom-8 w-3/4'>
