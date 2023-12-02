c20054d2aa8f95257746708ae52f155e3e7628b1
Vishnu Kumar <vermaji1891@gmail.com>
2023-12-02 12:17:38 +0530

    added pie chart

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,14 +3,28 @@ import { GrInProgress } from "react-icons/gr";
 import { MdPending  } from "react-icons/md";
 import { MdOutlineCancel   } from "react-icons/md";
 import { IoCheckmarkDoneCircle     } from "react-icons/io5";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, Title } from "chart.js";
+import { Pie } from 'react-chartjs-2';
 
 import Card from "../../components/Card";
 import HomeLayout from "../../layouts/HomeLayout";
 import useTicket from "../../hooks/useTickets";
 
+
+ChartJS.register(ArcElement, Tooltip, Legend, Title);
+
 function Home () {
 
     const [ticketsState] = useTicket()
+    const pieChartData = {
+        labels: Object.keys(ticketsState.ticketDistribution),
+        datasets: [
+            {
+                data: Object.values(ticketsState.ticketDistribution),
+                backgroundColor: ["red", "yellow", "blue", "orange", "green"]
+            }
+        ]
+    }
 
     return (
         <HomeLayout>
@@ -31,7 +45,12 @@ function Home () {
                     <MdOutlineCancel  className='inline mr-2' />
                 </Card>
             </div>
-            
+            <div className=" mt-10 flex justify-center items-center">
+                <div className="w-[22rem] h-[22rem]">
+                    <Pie data={pieChartData}/>
+                </div>
+            </div>
+
         </HomeLayout>
     )
 }
