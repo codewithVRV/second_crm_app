90dd713ecc3b15b53b2f6f2c88675aef919c9714
Vishnu Kumar <vermaji1891@gmail.com>
2023-12-04 13:29:12 +0530

    added generic function to update status & type

--- src/components/UserDisplayModal.jsx
+++ src/components/UserDisplayModal.jsx
@@ -5,13 +5,17 @@ import toast from "react-hot-toast";
 function UserDisplayModal ({user, resetTable}) {
     const [userDisplay, setUserDisplay] = useState(user)
 
-    async function handleStatusType (e) {
-        const dropDownStatus = document.getElementById("statusType")
-        dropDownStatus.open = !dropDownStatus.open;
+    async function handleUserType (e) {
+        
         try{
+
+            const ul = e.target.parentNode.parentNode;
+            const name = ul.getAttribute("name")
+            const dropDownStatus = document.getElementById(`${name}Dropdown`)
+            dropDownStatus.open = !dropDownStatus.open;
             const response = await axiosInstance.patch("user/updateUser", {
                 userId: userDisplay.id,
-                updates: {...userDisplay, userStatus: e.target.textContent.toLowerCase()}
+                updates: {...userDisplay, [name]: e.target.textContent.toLowerCase()}
             }, {
                 headers: {
                     "x-access-token" : localStorage.getItem("token")
@@ -38,15 +42,26 @@ function UserDisplayModal ({user, resetTable}) {
                         <h3 className="font-bold text-lg mt-0 text-center">User Details</h3>
                         <p className="py-[2px]">Name :- <span className="text-indigo-600 text-xl mx-5 font-semibold">{userDisplay.name}</span></p>
                         <p className="py-[2px]">Email:- <span className="text-indigo-600 text-xl mx-5 font-semibold">{userDisplay.email}</span></p>
-                        <p className="py-[2px]">Type:-  <span className="text-indigo-600 text-xl mx-5 font-semibold">{userDisplay.userType}</span></p>
+                        <p className="py-[2px]">Type:- 
+
+                         {/* <span className="text-indigo-600 text-xl mx-5 font-semibold">{userDisplay.userType}</span> */}
+                         <details className="dropdown" id="userTypeDropdown">
+                            <summary className="m-1 btn bg-gray-700 text-white">{userDisplay.userType}</summary>
+                            <ul name="userType" onClick={handleUserType} className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                                <li><a>Customer</a></li>
+                                <li><a>Admin</a></li>
+                                <li><a>Engineer</a></li>
+                            </ul>
+                        </details>
+                        </p>
                         <p className="py-[2px]">Status:-
                           {/* <span className="text-indigo-600 text-xl mx-5 font-semibold">{userDisplay.userStatus}</span> */}
-                          <details className="dropdown" id="statusType">
+                          <details className="dropdown" id="userStatusDropdown">
                                 <summary className="m-1 btn bg-gray-700 text-white">{userDisplay.userStatus}</summary>
-                                <ul onClick={handleStatusType} className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                                    <li><a>Approved</a></li>
+                                <ul name="userStatus" onClick={handleUserType} className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
                                     <li><a>Suspended</a></li>
                                     <li><a>Rejected</a></li>
+                                    <li><a>Approved</a></li>
                                 </ul>
                             </details>
                          </p>
